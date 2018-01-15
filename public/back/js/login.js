e55e86910477d4f70eb78e09dab4f2411bf70d98
xiaqianxia <xia@qianxia.com>
2018-01-15 21:52:11 +0800

    校验完

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -3,7 +3,10 @@
  */
 $(function() {
   var $form = $("form");
+  //表单校验插件 在表单提交的时候做校验
+  //如果失败了 会阻止表单的提交 如果校验成了 他就会提交
 
+  //表单插件初始化
   $form.bootstrapValidator({
     //  用户名不能为空 密码不能为空  长度是6-12位
     feedbackIcons:{
@@ -20,6 +23,9 @@ $(function() {
           notEmpty: {
             //  非空校验
             message: "用户名不能为空"
+          },
+          callback:{
+            message:"用户名错误"
           }
         }
       },
@@ -28,19 +34,58 @@ $(function() {
           notEmpty: {
             message: "密码不能为空哦"
           },
+
           //长度校验
           stringLength: {
             min: 6,
             max: 12,
             message: "密码必须是6-12为哦"
-          }
+          },
+
+          //用于校验失败后提示的信息
+
+          callback:{
+            message:"密码输入错误"
+          },
         }
       }
     }
 
-    
-  });
 
+  });
+//需要给表单注册一个校验成功的诗句 这个事件在表单校验成功的时候触发
+  $form.on("success.form.bv",function(e){
+  //  阻止表单提交
+    e.preventDefault();
+
+  //  发送ajax请求
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      dataType:"json",//注册dataType:jquery 会根据后端返回的类型自动识别
+      data:$form.serialize(),
+      success:function(info){
+      //  登陆成功
+        if(info.success){
+          location.href = "index.html";
+        }
+        //失败
+        if(info.error == 1000){
+          //updatastatus 方法 把用户名改成失败即可
+          //用于获取插件的使用 通过这个实例可以调用方法
+          //3个参数
+
+          $form.data("bootstrapValidator").updateStatus("username","INVALID","callback");
+          //alert(info.message);
+        }
+        if(info.error== 1001){
+          //alert(info.message);
+          $form.data("bootstrapValidator").updateStatus("password","INVALID","callback");
+        }
+      }
+    });
+
+  });
 
 
 
